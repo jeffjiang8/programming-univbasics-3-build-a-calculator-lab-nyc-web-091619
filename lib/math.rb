fea80f8163e98d259c156d292174f4cd358885ae
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-30 18:37:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,19 +5,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  puts quotient
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
